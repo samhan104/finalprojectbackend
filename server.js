7566e2ab72fedde99b32f51dc41a309815d6d6a4
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-22 21:34:57 -0500

    I have a working bot wow

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
 require('dotenv').config();
-const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require('discord.js')
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js')
+const fetch = require('node-fetch')
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-const prefix = '!';
 
 client.once("ready", () => {
     console.log("I'm alive and kicking")
 
-    client.user.setActivity(`this is a test`, {type: "WATCHING"})
+    client.user.setActivity(`this is a test`, { type: "WATCHING" })
 })
 
 
-client.on("messageCreate", (message) => {
-    if (!message.author.bot) {
-        console.log(`[${message.author.tag}]: ${message.content}`)
-    }
-    if (message.author.bot || !message.content.startsWith(prefix)) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const getCommand = args.shift().toLowerCase();
+client.on("messageCreate", async (message) => {
+    if (message.author.bot || !message.content.startsWith("!quiz")) return;
+    if (message.content.toLowerCase().startsWith("!quiz")) {
+        const response = await fetch("https://shielded-everglades-02667.herokuapp.com/discord");
+        const data = await response.json();
+        const length = data.length;
+        const questions = data[Math.floor(Math.random()*length)]
 
-    const messageArray = message.content.split(" ")
-    const argument = messageArray.slice(1);
 
-    if (getCommand === 'beepbeep') {
-        message.reply(`Beep beep, I'm a sheep. Beep Beep I'm a sheep`)
+        const filter = response => {
+            return questions.answer.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+        }
+
+
+        message.channel.send({ content: questions.question, fetchReply: true })
+            .then(() => {
+                message.channel.awaitMessages({ filter, max: 1, time: 10000, errors: ['time'] })
+                    .then(collected => {
+                        message.channel.send(`${collected.first().author} got the correct answer!`);
+                    })
+                    .catch(collected => {
+                        message.channel.send('Looks like nobody got the answer this time.');
+                    });
+            });
     }
-    console.log(`[${message.author.tag}]: ${message.content}`)
 })
+
 
 client.login(process.env.DISCORD_BOT_TOKEN)
